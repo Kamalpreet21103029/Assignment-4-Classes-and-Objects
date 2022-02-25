67be48af4cb074fb6ec1aaee480999d42a6ecc5e
Kamalpreet21103029 <97586578+Kamalpreet21103029@users.noreply.github.com>
2022-02-25 19:28:54 +0530

    Add files via upload

---
+++ Assignment 4 Recursion,Classes and Objects Output.py
@@ -0,0 +1,121 @@
+Python 3.2.3 (default, Apr 11 2012, 07:12:16) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+>>>>>>>>>>>>>>>>>>>Q1<<<<<<<<<<<<<<<<<<<<<
+The Solution for solving the Tower of Hanoi Problem for Three disks
+Move disk 1 from source A to destination B
+Move disk 2 from source A to destination C
+Move disk 1 from source B to destination C
+Move disk 3 from source A to destination B
+Move disk 1 from source C to destination A
+Move disk 2 from source C to destination B
+Move disk 1 from source A to destination B
+---------------------+++++++++++++++------------------------
+>>>>>>>>>>>>>>>>>>>Q2<<<<<<<<<<<<<<<<<<<<<
+Enter the number upto which we want to print the Pascal Triangle=11
+[1.0]
+[1.0, 1.0]
+[1.0, 2.0, 1.0]
+[1.0, 3.0, 3.0, 1.0]
+[1.0, 4.0, 6.0, 4.0, 1.0]
+[1.0, 5.0, 10.0, 10.0, 5.0, 1.0]
+[1.0, 6.0, 15.0, 20.0, 15.0, 6.0, 1.0]
+[1.0, 7.0, 21.0, 35.0, 35.0, 21.0, 7.0, 1.0]
+[1.0, 8.0, 28.0, 56.0, 70.0, 56.0, 28.0, 8.0, 1.0]
+[1.0, 9.0, 36.0, 84.0, 126.0, 126.0, 84.0, 36.0, 9.0, 1.0]
+[1.0, 10.0, 45.0, 120.0, 210.0, 252.0, 210.0, 120.0, 45.0, 10.0, 1.0]
+[1.0, 11.0, 55.0, 165.0, 330.0, 462.0, 462.0, 330.0, 165.0, 55.0, 11.0, 1.0]
+---------------------+++++++++++++++------------------------
+>>>>>>>>>>>>>>>>>>>Q3<<<<<<<<<<<<<<<<<<<<<
+Enter the Dividend=50
+Enter the Divisor=10
+The Quotient obtained is= 5
+The Remainder obtained is= 0
+The List of the output obtained is= [5, 0]
+The Divmod Function is not Callable
+All the values are not nonzero
+After adding 4,5 and 6 to our result gives [5, 0, 4, 5, 6]
+Converting the List to Set Datatype
+The Set1 is= {0, 4, 5, 6}
+The Required Set is= {4, 5, 6}
+To make the Set immutable, we have used frozenset function
+The maximum value in the Set is= 6
+The Hashed value of the maximum value obtained from the Set is= 6
+The Inbuilt Python Hashing function generally returns the same integer value which is given as input 
+---------------------+++++++++++++++------------------------
+>>>>>>>>>>>>>>>>>>>Q4<<<<<<<<<<<<<<<<<<<<<
+The object is created
+And the values reference's provided through the object to the class has been stored
+Student name= Ram
+Student Roll Number 21103000
+The object is created
+And the values reference's provided through the object to the class has been stored
+Student name= Shyam
+Student Roll Number 21103001
+The object is created
+And the values reference's provided through the object to the class has been stored
+Student name= Lakshman
+Student Roll Number 21103002
+Destructor is called
+The object and assignments made through it are deleted
+The Code ends
+---------------------+++++++++++++++------------------------
+>>>>>>>>>>>>>>>>>>>Q5<<<<<<<<<<<<<<<<<<<<<
+Employee Name= Mehak
+Employee Salary= 40000
+
+
+Employee Name= Ashok
+Employee Salary= 50000
+
+
+Employee Name= Viren
+Employee Salary= 60000
+
+
+Updating the Salary of The Employee Mehak
+Employee Name= Mehak
+Employee Salary= 70000
+
+
+Employee Name= Ashok
+Employee Salary= 50000
+
+
+Employee Name= Viren
+Employee Salary= 60000
+
+
+Deleting the record of the Employee Viren
+Employee Name= Mehak
+Employee Salary= 70000
+
+
+Employee Name= Ashok
+Employee Salary= 50000
+
+
+On Doing emp3.Info(), we will receive an error message
+The error states that  emp3.Info()  
+ NameError: name 'emp3' is not defined
+We receive an Error as we are trying to use a class function on the object emp3
+But by using del,that object has been deleted
+As it does not exists,but still we are trying to use it so we get an error
+---------------------+++++++++++++++------------------------
+>>>>>>>>>>>>>>>>>>>Q6<<<<<<<<<<<<<<<<<<<<<
+-----------------1st way to do it------------------------
+Friendship Test
+George and Barbie-Make Sure to enter a Meaningful word
+George Enter a Word=Hello
+Barbie Enter a Word=No
+Barbie and George are Fake Friends
+---------------------+++++++++++++++------------------------
+-----------------2nd way to do it------------------------
+Friendship Test
+George and Barbie-Make Sure to enter a Meaningful word
+George Enter a Word=Hello
+Barbie Enter a Word=No
+Barbie and George are Fake Friends
+---------------------+++++++++++++++------------------------
+>>> 
